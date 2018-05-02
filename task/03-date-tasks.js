bd62c0451aa4cd0360299267fd1347fb1780b294
Diana Arabey <diana.arabey@mail.ru>
2018-05-02 07:57:00 +0300

    Finished

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-    throw new Error('Not implemented');
+    return new Date(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-    throw new Error('Not implemented');
+  return new Date(value);
 }
 
 
@@ -56,7 +56,12 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-    throw new Error('Not implemented');
+  let year = date.getFullYear();
+    if (year % 100) {
+        return year % 4 == 0;
+    } else {
+        return year % 400 == 0;
+    }
 }
 
 
@@ -76,14 +81,14 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-    throw new Error('Not implemented');
+    return new Date(endDate - startDate).toISOString().slice(11,23);
 }
 
 
 /**
  * Возвращает угол (в радианах) между часовыми стрелками двух аналоговых часов для указанного времени по Гринвичу.
  * При возникновениии проблем, посмотрите : https://en.wikipedia.org/wiki/Clock_angle_problem
- * 
+ *
  * @param {date} date
  * @return {number}
  *
@@ -94,7 +99,11 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+  let hours = date.getUTCHours() % 12;
+  let min = date.getUTCMinutes();
+  let grad = Math.abs(0.5 * (60 * hours - 11 * min));
+  let rad = grad / 180 * Math.PI;
+  return rad <= Math.PI ? rad : rad - Math.PI;
 }
 
 
